bde9b079f473dc236a9bb4ed6781a0514a548da4
Maasaki Yamagishi <maasaki-yamagishi@mamezou.com>
2026-09-10 16:52:41 +0900

    確認に変更

--- facility-reservation-system/src/main/java/com/example/dockerpractice/kakunin.java
+++ facility-reservation-system/src/main/java/com/example/dockerpractice/kakunin.java
@@ -3,6 +3,6 @@ package com.example.dockerpractice;
 public class kakunin {
     
     public void kakunin () {
-       System.out.println("プッシュ確認用のコード");
+       System.out.println("確認");
     };
 }
